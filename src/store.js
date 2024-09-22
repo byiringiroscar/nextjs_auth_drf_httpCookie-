518d33b902cc090d43b246ae7eff98a10b21282d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-09-22 14:34:15 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -6,3 +6,36 @@ import reducers from './reducers';
 
 
 let store;
+
+function initStore(initialState) {
+    return createStore(
+        reducers,
+        initialState,
+        composeWithDevTools(applyMiddleware(thunkMiddleware))
+    );
+}
+
+export const initalizeStore = (preloadedState) => {
+    let _store = store ?? initStore(preloadedState);
+    // afater navigating to a page with an initial redux state, merge that state
+    // with the current state in the store and create a new store
+    if (preloadedState && store) {
+        _store = initStore({
+            ...store.getState(),
+            ...preloadedState,
+        });
+        // reset the current store
+        store = undefined;
+    }
+    // for SSG and SSR always create a new store
+    if (typeof window === "undefined") return _store;
+    //  Create the store once in the client
+    if (!store) store = _store;
+
+    return _store;
+}
+
+export function useStore(initialState) {
+    const store = useMemo(() => initalizeStore(initialState), [initialState]);
+    return store;
+}
